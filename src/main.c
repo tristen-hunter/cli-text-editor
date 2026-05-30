47363e986f8f7c2d43ae08b31009f831d9393fca
tristen-hunter <tristenhunter0702@gmail.com>
2026-05-30 09:33:24 +0200

    feat: added key handling for END, HOME, PgUp and PgDown

--- src/main.c
+++ src/main.c
@@ -17,7 +17,12 @@ enum editorKey {
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
-    ARROW_DOWN
+    ARROW_DOWN,
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 /*** data ***/
@@ -66,22 +71,40 @@ int editorReadKey() {
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
         if (nread == -1 && errno != EAGAIN) die("read");
     }
-
     if (c == '\x1b') {
         char seq[3];
-
         if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
         if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
-
         if (seq[0] == '[') {
+        if (seq[1] >= '0' && seq[1] <= '9') {
+            if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+            if (seq[2] == '~') {
             switch (seq[1]) {
-                case 'A': return ARROW_UP;
-                case 'B': return ARROW_DOWN;
-                case 'C': return ARROW_RIGHT;
-                case 'D': return ARROW_LEFT;
+                case '1': return HOME_KEY;
+                case '3': return DEL_KEY;
+                case '4': return END_KEY;
+                case '5': return PAGE_UP;
+                case '6': return PAGE_DOWN;
+                case '7': return HOME_KEY;
+                case '8': return END_KEY;
+            }
+            }
+        } else {
+            switch (seq[1]) {
+            case 'A': return ARROW_UP;
+            case 'B': return ARROW_DOWN;
+            case 'C': return ARROW_RIGHT;
+            case 'D': return ARROW_LEFT;
+            case 'H': return HOME_KEY;
+            case 'F': return END_KEY;
             }
         }
-
+        } else if (seq[0] == 'O') {
+        switch (seq[1]) {
+            case 'H': return HOME_KEY;
+            case 'F': return END_KEY;
+        }
+        }
         return '\x1b';
     } else {
         return c;
@@ -197,16 +220,24 @@ void editorRefreshScreen() {
 void editorMoveCursor(int key) {
   switch (key) {
     case ARROW_LEFT:
-      E.cx--;
+        if (E.cx != 0) {
+            E.cx--;
+        }
       break;
     case ARROW_RIGHT:
-      E.cx++;
-      break;
+        if (E.cx != E.screencols - 1) {
+            E.cx++;
+        }
+        break;
     case ARROW_UP:
-      E.cy--;
-      break;
+        if (E.cy != 0) {
+            E.cy--;
+        }
+        break;
     case ARROW_DOWN:
-      E.cy++;
+        if (E.cy != E.screenrows - 1) {
+            E.cy++;
+        }
       break;
   }
 }
@@ -219,6 +250,24 @@ void editorProcessKeypress() {
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+
+    case HOME_KEY:
+        E.cx = 0;
+        break;
+
+    case END_KEY:
+        E.cx = E.screencols - 1;
+        break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+        {
+            int times = E.screenrows;
+            while (times--)
+                editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+        }
+        break;
+
 
     case ARROW_UP:
     case ARROW_DOWN:
