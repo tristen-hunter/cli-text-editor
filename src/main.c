46684a4c98b2332959e51b175ad4d0c6f7555155
tristen-hunter <tristenhunter0702@gmail.com>
2026-05-30 08:29:18 +0200

    feat: editor is initialzed by calling sys for terminal size
    feat: get the cursor position

--- src/main.c
+++ src/main.c
@@ -13,7 +13,9 @@
 
 /*** data ***/
 struct editorConfig {
-  struct termios E.orig_termios;
+    int screenrows;
+    int screencols;
+  struct termios orig_termios;
 };
 struct editorConfig E;
 
@@ -57,11 +59,31 @@ char editorReadKey() {
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
-    if (ioctl(STDOUT_FILENO, TIOCWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
@@ -73,8 +95,8 @@ int getWindowSize(int *rows, int *cols) {
 
 void editorDrawRows() {
     int y;
-    for (y = 0; y < 24; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3)
+    for (y = 0; y < E.screenrows; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
     }
 }
 
@@ -84,7 +106,7 @@ void editorRefreshScreen() {
 
     editorDrawRows();
 
-    write(STDOUT_FILENO, "\x1b[H", 3)
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 
@@ -103,8 +125,13 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main(){
     enableRawMode();
+    initEditor();
 
     while(1) {
         editorRefreshScreen();
